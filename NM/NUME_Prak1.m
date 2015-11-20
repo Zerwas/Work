59da7a0da9a063c2d285ea1ab73736df77b7ebd7
Florian Starke <f_starke@gmx.de>
2015-11-20 19:56:12 +0100

    NUME Plou

--- NM/NUME_Prak1.m
+++ NM/NUME_Prak1.m
@@ -1,8 +1,16 @@
 fR = @(x) 1./(1.+25.*x.^2);
 f1 = @(x) (1.+cos(3./2.*pi.*x)).^(2/3);
 N = 12;
-x = [-1:1/(10*N):1];
+x = [-1:2/N:1];
 y = fR(x);
-%plot(x,y);
+[P] = polyfit(x,y,N+1);
+
+xPlot = [-1:2/(10*N):1];
+
+yPlot = fR(xPlot);
+polyPlot = polyval(P,xPlot);
+
+plot(xPlot,polyPlot,xPlot,yPlot)
+
 
 
