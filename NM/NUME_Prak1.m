e0fec621ad9cb3f8edfd7b21339315f8acb6507b
Florian Starke <f_starke@gmx.de>
2015-11-23 10:09:22 +0100

    funktions

--- NM/NUME_Prak1.m
+++ NM/NUME_Prak1.m
@@ -1,22 +1,19 @@
-N = 12;
+function[] = NUME_Prak1(f,N)
+	%fR = @(x) 1./(1.+25.*x.^2);
+	%f1 = @(x) (1.+cos(3/2.*pi.*x)).^(2/3);
+	T  = @(k) cos((2.*k.+1)./(2*N).*pi);
 
-fR = @(x) 1./(1.+25.*x.^2);
-f1 = @(x) (1.+cos(3/2.*pi.*x)).^(2/3);
-T  = @(k) cos((2.*k.+1)./(2*N).*pi);
+	x = [-1:2/N:1];
+	xTsch = T([1:1:N]);
 
+	y = f(x);
+	[P] = polyfit(x,y,N+1);
 
-x = [-1:2/N:1];
-xTsch = T([1:1:N]);
+	xPlot = [-1:2/(10*N):1];
 
-y = fR(x);
-[P] = polyfit(x,y,N+1);
+	yPlot = f(xPlot);
+	polyPlot = polyval(P,xPlot);
 
-xPlot = [-1:2/(10*N):1];
-
-yPlot = fR(xPlot);
-polyPlot = polyval(P,xPlot);
-
-plot(xPlot,polyPlot,xPlot,yPlot)
-
-
-
+	plot(xPlot,polyPlot,xPlot,yPlot);
+	return
+endfunction
